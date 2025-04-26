0e19ee07cab5e50f96fee3302c8b3e6314a3a479
cr3wcr3w <racelisjk@gmail.com>
2025-04-26 13:01:39 +0800

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+
+serve(
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		// biome-ignore lint/suspicious/noConsole: <explanation>
+		console.log(
+			`Server is running on http://localhost:${info.port} ~main branch`,
+		);
+	},
+);
