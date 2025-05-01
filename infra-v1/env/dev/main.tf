58787d3289d3ef39fc8efa9081ed09a4b5522eef
cr3wcr3w <racelisjk@gmail.com>
2025-05-01 18:15:54 +0800

    refactor: comment out unused database and space bucket resources in Terraform files

--- infra-v1/env/dev/main.tf
+++ infra-v1/env/dev/main.tf
@@ -14,22 +14,22 @@ resource "digitalocean_droplet" "backend-1" {
 }
 
 # Create Database
-resource "digitalocean_database_postgresql_config" "postgres-1-config" {
-  cluster_id = digitalocean_database_cluster.postgres-1.id
-  timezone   = "UTC"
-}
-resource "digitalocean_database_cluster" "postgres-1" {
-  name       = var.digitalocean_database_cluster_name
-  engine     = var.digitalocean_database_cluster_engine
-  version    = var.digitalocean_database_cluster_version
-  size       = var.digitalocean_database_cluster_size
-  region     = var.digitalocean_region
-  node_count = var.digitalocean_database_cluster_node_count
-}
+# resource "digitalocean_database_postgresql_config" "postgres-1-config" {
+#   cluster_id = digitalocean_database_cluster.postgres-1.id
+#   timezone   = "UTC"
+# }
+# resource "digitalocean_database_cluster" "postgres-1" {
+#   name       = var.digitalocean_database_cluster_name
+#   engine     = var.digitalocean_database_cluster_engine
+#   version    = var.digitalocean_database_cluster_version
+#   size       = var.digitalocean_database_cluster_size
+#   region     = var.digitalocean_region
+#   node_count = var.digitalocean_database_cluster_node_count
+# }
 
 # Create Space Bucket
-resource "digitalocean_spaces_bucket" "bucket" {
-  name   = var.digitalocean_spaces_bucket_name
-  region = var.digitalocean_region
-  acl = var.digitalocean_spaces_bucket_acl
-}
+# resource "digitalocean_spaces_bucket" "bucket" {
+#   name   = var.digitalocean_spaces_bucket_name
+#   region = var.digitalocean_region
+#   acl = var.digitalocean_spaces_bucket_acl
+# }
