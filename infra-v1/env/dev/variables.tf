821f63930757a9024ebda973da4d21120481ec4a
cr3wcr3w <racelisjk@gmail.com>
2025-05-01 18:16:16 +0800

    refactor: comment out unused database and space bucket variable definitions in Terraform

--- infra-v1/env/dev/variables.tf
+++ infra-v1/env/dev/variables.tf
@@ -22,26 +22,26 @@ variable "digitalocean_droplet_size" {
 }
 
 # variable For Database
-variable "digitalocean_database_cluster_name" {
-    default = "postgres-1-dev"
-}
-variable "digitalocean_database_cluster_engine" {
-    default = "pg"
-}
-variable "digitalocean_database_cluster_version" {
-    default = "15"
-}
-variable "digitalocean_database_cluster_size" {
-    default = "db-s-1vcpu-1gb"
-}
-variable "digitalocean_database_cluster_node_count" {
-    default = 1
-}
+# variable "digitalocean_database_cluster_name" {
+#     default = "postgres-1-dev"
+# }
+# variable "digitalocean_database_cluster_engine" {
+#     default = "pg"
+# }
+# variable "digitalocean_database_cluster_version" {
+#     default = "15"
+# }
+# variable "digitalocean_database_cluster_size" {
+#     default = "db-s-1vcpu-1gb"
+# }
+# variable "digitalocean_database_cluster_node_count" {
+#     default = 1
+# }
 
 # variable For Space Bucket
-variable "digitalocean_spaces_bucket_name" {
-    default = "backend-bucket-dev"
-}
-variable "digitalocean_spaces_bucket_acl" {
-    default = "private"
-}
+# variable "digitalocean_spaces_bucket_name" {
+#     default = "backend-bucket-dev"
+# }
+# variable "digitalocean_spaces_bucket_acl" {
+#     default = "private"
+# }
